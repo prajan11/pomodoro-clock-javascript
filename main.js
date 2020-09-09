09ff11a593a90fd2e0c1f4f4b14e4694ca6250f3
Prajan Acharya <prajanacharya11@gmail.com>
2020-09-09 17:31:21 +0545

    Alarm sound on changing the time state + Implementation of Pomodoro Logic

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let break_length = 5;
 let duration_type = "Session";
 let timer_run_status = false;
 let session_time = true;
+let time_type;
 let intervalFunction;
+let timer_status = "normal";
 
 document.getElementById('session-length').innerHTML = session_length;
 document.getElementById('break-length').innerHTML = break_length;
@@ -14,14 +16,18 @@ document.getElementById('duration-type').innerHTML = duration_type;
 
 //for incrementing and decrementing the session lengths
 let regulateSessionLength = (operation_type) => {
-    if(timer_run_status){
+    if(timer_status === "session_active"){
         return;
     }
     
-    if(operation_type == "plus"){
+    if(timer_run_status){
+        return;
+    }
+   
+    if(operation_type === "plus"){
         session_length++; 
     }
-    else if(operation_type == "minus"){
+    else if(operation_type === "minus"){
         if(session_length <= 1){
             return;
         }
@@ -29,20 +35,30 @@ let regulateSessionLength = (operation_type) => {
     }
 
     document.getElementById('session-length').innerHTML = session_length;
-    document.getElementById('minutes').innerHTML = session_length;
+    if(timer_status === "normal"){
+        document.getElementById('minutes').innerHTML = session_length;
+    }
+   
 }
 
 //for incrementing and decrementing the break lengths
 let regulateBreakLength = (operation_type) => {
 
+    if(timer_status == "break_active"){
+        return;
+    }
+  
+
     if(timer_run_status){
         return;
     }
+
     
-    if(operation_type == "plus"){
+    
+    if(operation_type === "plus"){
         break_length++; 
     }
-    else if(operation_type == "minus"){
+    else if(operation_type === "minus"){
         if(break_length <= 1){
             return;
         }
@@ -58,6 +74,7 @@ let resetTime = () => {
     clearInterval(intervalFunction);
 
     timer_run_status = false;
+    timer_status = "normal";
 
     session_length = 30;
     break_length = 5;
@@ -94,6 +111,7 @@ let regulateTimer = () => {
 let updateTimeEverySecond = () => {
     //if session time is active
     if(session_time){
+        timer_status = "session_active";
         let total_seconds = session_length * 60 - 1 ;
         if(total_seconds >= 0){
             let minutes = Math.floor(total_seconds / 60);
@@ -104,7 +122,11 @@ let updateTimeEverySecond = () => {
             session_length = minutes + (seconds / 60);
         }
         else{  //for updating break time for a second after session time becomes 0
+            playAudio();  //for playing alarm
+
+            break_length = Number(document.getElementById('break-length').textContent);
             session_time = false;
+            timer_status = "break_active";
             duration_type="Break";
             total_seconds = break_length * 60 - 1 ;
     
@@ -120,6 +142,7 @@ let updateTimeEverySecond = () => {
     }
     else{  //if break time is active
         let total_seconds = break_length * 60 - 1 ;
+        timer_status = "break_active";
         if(total_seconds >= 0){
             let minutes = Math.floor(total_seconds / 60);
             let seconds = Math.floor(total_seconds) - minutes * 60;
@@ -129,8 +152,11 @@ let updateTimeEverySecond = () => {
             break_length = minutes + (seconds / 60);
         }
         else{   //for updating session time for a second after break time becomes 0
+            playAudio(); //for playing alarm
+
             session_length = Number(document.getElementById('session-length').textContent);
             session_time = true;
+            timer_status = "session_active";
             duration_type="Session";
             total_seconds = session_length * 60 - 1 ;
     
@@ -144,9 +170,10 @@ let updateTimeEverySecond = () => {
            
         }
     }
-   
-   
     
+}
 
-    
+let playAudio = () => {
+    var audio = new Audio('alarm-sound.mp3');
+    audio.play();
 }
